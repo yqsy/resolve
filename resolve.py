6a5bd4540f449b8b5cab1519dc52b1897d976900
Guochun.Gong <yqsy021@126.com>
2017-07-26 13:56:25 +0800

    强大的DictReader

--- resolve.py
+++ resolve.py
@@ -2,6 +2,7 @@ import csv
 
 if __name__ == '__main__':
     with open('03_example.csv', encoding='utf-8-sig') as csvfile:
-        spamreader = csv.reader(csvfile)
-        for row in spamreader:
-            print(','.join(row))
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            print(row)
+
