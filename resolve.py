052cf5e731f1de02fa448660bafd03151ab84003
Guochun.Gong <yqsy021@126.com>
2017-07-26 13:47:42 +0800

    最简易的读取方式,  csv.reader

--- resolve.py
+++ resolve.py
@@ -1,2 +1,7 @@
+import csv
+
 if __name__ == '__main__':
-    pass
+    with open('03_example.csv', encoding='utf-8-sig') as csvfile:
+        spamreader = csv.reader(csvfile)
+        for row in spamreader:
+            print(','.join(row))
