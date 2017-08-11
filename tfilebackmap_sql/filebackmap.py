6814b8fb80ec2cdfae897ff0841da5dbc16345de
Guochun.Gong <yqsy021@126.com>
2017-08-11 16:21:50 +0800

    rename

---
+++ tfilebackmap_sql/filebackmap.py
@@ -0,0 +1,52 @@
+import csv
+
+import argparse
+
+from collections import OrderedDict
+
+PARSER = argparse.ArgumentParser()
+
+PARSER.add_argument('csvfile', nargs=1, help='which csv file to be converted'
+                                             'to sql')
+
+PARSER.add_argument('--tacode', dest='tacode', action='store', required=True)
+
+PARSER.add_argument('--filetype', dest='filetype', action='store', required=True)
+
+
+def sql_generator(filename, tacode, filetype):
+    with open(filename, encoding='utf-8-sig') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        for row_dict in reader:
+            sorted_row = OrderedDict(sorted(row_dict.items(),
+                                            key=lambda item: reader.fieldnames.index(item[0])))
+
+            insert_str = '''INSERT INTO dbo.TFILEBACKMAP (FTACODE, FFLTYPE, FFLNAME, FBKNAME, FSPECIA)
+VALUES ('{FTACODE}', '{FFLTYPE}', '{FFLNAME}', '{FBKNAME}', '')
+GO
+
+'''
+            for key, value in sorted_row.items():
+                sorted_row[key] = value.strip()
+
+            yield insert_str.format(FTACODE=tacode, FFLTYPE=filetype,
+                                    FFLNAME=sorted_row['field'], FBKNAME=sorted_row['back_field'])
+
+
+def main():
+    options = PARSER.parse_args()
+    filename = options.csvfile[0]
+
+    sql_func = sql_generator(filename, options.tacode, options.filetype)
+
+    out_file_name = '{}-{}-TFILEFIELD.sql'.format(options.tacode, options.filetype)
+    with open(out_file_name, 'w') as out_file:
+        for sql_str in sql_func:
+            out_file.write(sql_str)
+
+    print(out_file_name, ' created')
+
+
+if __name__ == '__main__':
+    main()
--- tfilefield_sql/readme.md
+++ tfilefield_sql/readme.md
@@ -1,9 +1,9 @@
 ```
-python to_sql.py 000-43.csv --tacode 000 --filetype 43
-python to_sql.py 000-44.csv --tacode 000 --filetype 44
-python to_sql.py 27-43.csv --tacode 27 --filetype 43
-python to_sql.py 27-44.csv --tacode 27 --filetype 44
+python filefield.py 000-43.csv --tacode 000 --filetype 43
+python filefield.py 000-44.csv --tacode 000 --filetype 44
+python filefield.py 27-43.csv --tacode 27 --filetype 43
+python filefield.py 27-44.csv --tacode 27 --filetype 44
 
-python to_sql.py 27-01.csv --tacode 27 --filetype 01
-python to_sql.py 27-03.csv --tacode 27 --filetype 03
+python filefield.py 27-01.csv --tacode 27 --filetype 01
+python filefield.py 27-03.csv --tacode 27 --filetype 03
 ```
